e8b7e5fe9e3d3ee37eaeccf501de6691b57c6b0b
NMS <nesiteam@gmail.com>
2026-02-14 22:39:50 +0900

    Initial Commit

---
+++ src/main/java/koza/licensemanagementservice/LicenseManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package koza.licensemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicenseManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/koza/licensemanagementservice/LicenseManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package koza.licensemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
