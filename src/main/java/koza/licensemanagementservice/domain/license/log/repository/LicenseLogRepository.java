c246a93070ced3358b54d1fa393aa4c6ab406f3b
NMS <nesiteam@gmail.com>
2026-04-14 23:11:40 +0900

    feat: Admin 전용 라이센스 변경로그 조회 API 추가

--- src/main/java/koza/licensemanagementservice/domain/license/log/repository/LicenseLogRepository.java
+++ src/main/java/koza/licensemanagementservice/domain/license/log/repository/LicenseLogRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LicenseLogRepository extends JpaRepository<LicenseLog, Long> {
+public interface LicenseLogRepository extends LicenseLogRepositoryCustom, JpaRepository<LicenseLog, Long> {
 }
--- src/main/java/koza/licensemanagementservice/domain/license/log/service/LicenseLogAdminService.java
+++ src/main/java/koza/licensemanagementservice/domain/license/log/service/LicenseLogAdminService.java
@@ -2,7 +2,10 @@ package koza.licensemanagementservice.domain.license.log.service;
 
 import koza.licensemanagementservice.auth.dto.CustomUser;
 import koza.licensemanagementservice.domain.license.log.dto.LicenseExtendLogResponse;
+import koza.licensemanagementservice.domain.license.log.dto.LicenseLogResponse;
 import koza.licensemanagementservice.domain.license.log.repository.LicenseExtendLogRepository;
+import koza.licensemanagementservice.domain.license.log.repository.LicenseLogRepository;
+import koza.licensemanagementservice.domain.license.log.repository.LicenseLogSearchCondition;
 import koza.licensemanagementservice.global.error.BusinessException;
 import koza.licensemanagementservice.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -11,17 +14,23 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class LicenseLogAdminService {
     private final LicenseExtendLogRepository extendLogRepository;
+    private final LicenseLogRepository logRepository;
 
     public Page<LicenseExtendLogResponse> getLicenseExtendLogs(CustomUser user, Long licenseId, LocalDate from, LocalDate to, Pageable pageable) {
         validAdminAuthorized(user);
 
         return extendLogRepository.findByLicenseId(licenseId, from, to, pageable);
+    }
+
+    public Page<LicenseLogResponse> getLicenseChangedLogs(CustomUser user, Long licenseId, LicenseLogSearchCondition condition, Pageable pageable) {
+        validAdminAuthorized(user);
+
+        return logRepository.findByLicenseId(licenseId, condition, pageable);
     }
 
     private static void validAdminAuthorized(CustomUser user) {
