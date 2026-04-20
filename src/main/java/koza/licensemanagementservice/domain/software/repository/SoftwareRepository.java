146bf92adf4924284b64ca25822f6e7bac67f582
NMS <nesiteam@gmail.com>
2026-04-20 23:19:18 +0900

    feat: 라이센스 인증 시 성공/실패 로그 추가, appId 인증 추가

--- src/main/java/koza/licensemanagementservice/domain/software/repository/SoftwareRepository.java
+++ src/main/java/koza/licensemanagementservice/domain/software/repository/SoftwareRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SoftwareRepository extends JpaRepository<Software, Long>, SoftwareRepositoryCustom {
+    Optional<Software> findByAppId(String appId);
     List<Software> findByMemberId(Long memberId);
 }
