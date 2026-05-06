06da36159a5193cc662837fbd6f57c9ee429f7d7
NMS <ny_nesi@naver.com>
2026-05-06 17:45:50 +0900

    feat(software): 목록 페이지 응답 필드 추가
    - 소프트웨어 상태(status), 상태 만료기간(statusUntil)

--- src/main/java/koza/licensemanagementservice/domain/software/dto/response/SoftwareSummaryResponse.java
+++ src/main/java/koza/licensemanagementservice/domain/software/dto/response/SoftwareSummaryResponse.java
@@ -1,6 +1,7 @@
 package koza.licensemanagementservice.domain.software.dto.response;
 
 import com.querydsl.core.annotations.QueryProjection;
+import koza.licensemanagementservice.domain.software.entity.SoftwareStatus;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,15 +13,19 @@ public class SoftwareSummaryResponse {
     private Long id;
     private String name;
     private String version;
+    private SoftwareStatus status;
+    private LocalDateTime statusUntil;
     private int licenseCount;
     private int activeSessionCount;
     private LocalDateTime createAt;
 
     @QueryProjection
-    public SoftwareSummaryResponse(Long id, String name, String version, int licenseCount, int activeSessionCount, LocalDateTime createAt) {
+    public SoftwareSummaryResponse(Long id, String name, String version, SoftwareStatus status, LocalDateTime statusUntil, int licenseCount, int activeSessionCount, LocalDateTime createAt) {
         this.id = id;
         this.name = name;
         this.version = version;
+        this.status = status;
+        this.statusUntil = statusUntil;
         this.licenseCount = licenseCount;
         this.activeSessionCount = activeSessionCount;
         this.createAt = createAt;
