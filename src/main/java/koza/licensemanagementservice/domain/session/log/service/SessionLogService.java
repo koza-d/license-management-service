28a1ec4388bf134d82e181c5a2404b02b41572cb
NMS <ny_nesi@naver.com>
2026-05-28 22:29:01 +0900

    fix(sessionLog): 최근 30일 세션 사용시간 조회 시 날짜 공백 채우기 추가

--- src/main/java/koza/licensemanagementservice/domain/session/log/service/SessionLogService.java
+++ src/main/java/koza/licensemanagementservice/domain/session/log/service/SessionLogService.java
@@ -8,12 +8,14 @@ import koza.licensemanagementservice.global.error.ErrorCode;
 import koza.licensemanagementservice.domain.license.entity.License;
 import koza.licensemanagementservice.domain.license.repository.LicenseRepository;
 import koza.licensemanagementservice.auth.dto.user.CustomUser;
+import koza.licensemanagementservice.global.util.FillGaps;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -27,7 +29,11 @@ public class SessionLogService {
     public List<DailyUsageResponse> getDailyUsageTime(CustomUser user, Long licenseId, int range) {
         getLicenseOrThrow(user, licenseId);
         LocalDateTime startDate = LocalDateTime.now().minusDays(range);
-        return logRepository.findDailyUsage(licenseId, startDate);
+        List<DailyUsageResponse> result = logRepository.findDailyUsage(licenseId, startDate);
+
+        return FillGaps.fillDateGaps(startDate.toLocalDate(), LocalDate.now(), result,
+                DailyUsageResponse::getDate, date -> new DailyUsageResponse(date, 0L)
+        );
    }
 
    public Page<SessionHistoryResponse> getLicenseUsageHistory(CustomUser user, Long licenseId, Pageable pageable) {
